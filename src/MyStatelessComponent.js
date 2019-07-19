3db8daa5c3476b78c898c9817158c3a5c5da5aef
peperpeach <peper_peach@hotmail.com>
2019-07-19 22:46:07 +0700

    Init

---
+++ src/MyStatelessComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const MyStatelessComponent = ({ title, message }) => (
+  <div>
+    <h1>{ title }</h1>
+    <p>{ message }</p>
+  </div>
+)
+
+export default MyStatelessComponent
